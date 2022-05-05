9ee505557497fe0788fe52b3802d696b030cd555
nikita <adrbrug@gmail.com>
2022-05-05 23:30:51 +0300

    Initial commit

---
+++ lesson1/js/lesson1.1.js
@@ -0,0 +1,45 @@
+"use strict";
+// 1. параметры инициализации компонента,
+// объявления свойств, за значениями которых будет следить
+// система реактивности vue, методы - обработчики событий и тд
+let rootComponentOption = {
+    // в компоненте data - метод, который возвращает объект
+    // свойства, перечисленные в объекте попадают в систему
+    // реактивности vue
+    data(){
+        return {
+            userInput: "",
+            book: {
+                title: "vue js",
+                author: "js developer",
+                otherAuthors: ["junior1", "junior2"],
+                img: "https://picsum.photos/id/44/200/300"
+            }
+        }
+    },
+    computed: { /* вычисляемые свойства */
+        userOutput(){ // метод, который возвращает значение свойства userOutput
+            // this - ссылка на текущий компонент!
+            // обращаться к свойствам из data нужно через this
+            return this.userInput.length < 1 ? "Вы ничего не ввели" : this.userInput;
+        },
+        userHtmlOutput(){
+            let res = this.userInput.length < 1 ? "Вы ничего не ввели" : this.userInput;
+            return `<span>${res}</span>`;
+        }
+    }
+};
+
+// создание приложения vue
+// фабричный метод createApp создает экземпляр vue (new Vue)
+let app = Vue.createApp(rootComponentOption);
+
+// монтирование приложения в html,
+// метод mount возвращает ссылку на корневой компонент приложения,
+// который и является связующим звеном между js и html (ViewModel)
+let vm = app.mount("#application");
+
+// Vue.createApp({
+//    настройки корневого компонента
+//
+// }).mount("#application");
