24030ff337abdcf5b21f4e8b035f91f7a42d344c
aud203 <aud203@gmail.com>
2022-05-17 22:11:25 +0300

    Занятие 5: файлы лекции ++

---
+++ lesson5/js/lesson5.js
@@ -0,0 +1,115 @@
+"use strict";
+
+const mainPage = {
+    template: "<div>Главная страница</div>"
+};
+const coursesPage = {
+    template:
+        `
+            <div>
+                <h2>Список курсов</h2>
+                <div v-for="course in courses">
+                    <h3>{{ course.title }}</h3>
+                    <!-- маршрут с параметрами -->
+                    <router-link :to="'/course/' + course.id">
+                      Подробнее
+                    </router-link>
+                </div>
+            </div>
+        `,
+    data(){
+        return {
+            courses: [
+                {id: 1, title: "JavaScript"},
+                {id: 2, title: "Vue Js"},
+                {id: 3, title: "React"},
+            ]
+        }
+    }
+};
+const oneCourse = {
+    template:
+        ` 
+        <div>
+            <h2>Подробная информация о курсе {{ $route.params.id }}</h2>
+            <div>
+                <router-link :to="'/course/' + $route.params.id + '/reviews'">
+                    Отзывы
+                </router-link>
+                <router-link :to="'/course/' + $route.params.id + '/works'">
+                    Примеры работ выпускников
+                 </router-link>
+            </div>
+            <router-view></router-view>
+        </div>
+        `
+    // при обращении из методов, вычисляемых свойств и тд:
+    // this.$route.param.id
+};
+
+const contactsPage = {
+    template: "<div>Страница с контактной информацией</div>"
+};
+
+const notFound = {
+    template: "<div>Страница не найдена</div>"
+};
+
+const reviews = {
+    template: "<div>Отзывы</div>"
+};
+
+const works = {
+    template: "<div>Работы выпускников</div>"
+};
+
+const app = Vue.createApp({});
+
+// настройка и подключение маршрутизатора к vue
+
+// 1. массив с маршрутами: связь между компонентамим и ссылками
+const routes = [
+    { // маршурт: связь между одной ссылкой и одним компонентом
+        path: "/", // ссылка
+        component: mainPage // компонент, который должен быть подгружен
+        // при переходе по ссылке из path
+    },
+    {
+        path: "/courses",
+        component: coursesPage
+    },
+    {
+        path: "/contacts",
+        component: contactsPage
+    },
+    { // маршрут с параметрами, динамические маршруты
+        path: "/course/:id(\\d+)",
+        component: oneCourse,
+        // вложенные маршруты
+        children: [
+            // значение атрибута to в router-link  /course/1/reviews
+            {path: "reviews", component: reviews},
+            // значение атрибута to в router-link  /course/1/works
+            {path: "works", component: works}
+        ]
+    },
+    {
+        path: "/:pathMatch(.*)*", // 0 или несколько любых символов
+        component: notFound
+    }
+];
+
+
+
+// 2. создание экземпляра маршрутизатора
+// createRouter принимает на вход объект с настройками
+// и возвращает экземпляр (объект) маршрутизатора
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes
+});
+
+// 3. связь маршрутизатора и vue
+app.use(router); // до вызова метода mount
+
+app.mount("#app");
